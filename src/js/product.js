5572805314b60c0ded5817c7bdc04605fc6e6019
migarcos <migark@hotmail.com>
2025-03-08 11:37:34 -0500

    solving individual task

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,35 @@
-import { setLocalStorage } from "./utils.mjs";
+// original line changed w01
+// import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // next line is the original
+  // setLocalStorage("so-cart", product);
+
+  let cart = getLocalStorage("so-cart");
+
+  if (!Array.isArray(cart)) {
+    cart = [];
+  }
+
+  // const inCarProd = cart.find((item) => item.Id === product.Id);
+
+  // if (inCarProd) {
+  //   inCarProd.Quantity += 1;
+  // } else {
+  //   product.Quantity = 1;
+
+  //   cart.push(product);
+  // }
+
+  cart.push(product); // add individual products 
+
+  setLocalStorage("so-cart", cart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
