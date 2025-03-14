3d4170cbcfb077f40502bf6453999e5da2beed6c
Manuel Flores <mfloresyanez@byupathway.edu>
2025-03-14 00:31:04 -0300

    feat: homework week 1 completed

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,15 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let localStorage = getLocalStorage("so-cart");
+  if (localStorage == undefined) {
+    localStorage = [];
+  }
+  localStorage.push(product);
+  setLocalStorage("so-cart", localStorage);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
