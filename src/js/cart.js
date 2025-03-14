5e72685fd1de49417f13582aff17e9a400ab0f2f
Morgan Allen <morganallen1999@gmail.com>
2025-03-14 15:16:26 -0600

    added logic to display total of items in cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,15 @@
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  const cartItems = getLocalStorage("so-cart") || [];
+
+  if (cartItems && cartItems.length > 0) {
+    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+    document.querySelector(".product-list").innerHTML = htmlItems.join("");
+    displayCartTotal(cartItems);
+  } else {
+    document.querySelector(".product-list").innerHTML = "";
+  }
 }
 
 function cartItemTemplate(item) {
@@ -23,6 +29,28 @@ function cartItemTemplate(item) {
 </li>`;
 
   return newItem;
+}
+
+function calculateTotal(cartItems) {
+  const total = cartItems.reduce((sum, item) => sum + item.FinalPrice, 0);
+
+  return total;
+}
+
+function displayCartTotal(cartItems) {
+  const cartFooter = document.querySelector(".cart-footer");
+  const cartTotalElement = document.querySelector(".cart-total");
+
+  if (cartItems && cartItems.length > 0) {
+    const total = calculateTotal(cartItems);
+
+    const formattedTotal = `Total: $${total.toFixed(2)}`;
+
+    cartTotalElement.textContent = formattedTotal;
+    cartFooter.classList.remove("hide");
+  } else {
+    cartFooter.classList.add("hide");
+  }
 }
 
 renderCartContents();
