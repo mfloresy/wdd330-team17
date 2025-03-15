b2d35049c333cd0304e3bf64d605d1d37ec7fa40
Sadly4343 <loganhartshorn@gmail.com>
2025-03-15 16:00:28 -0600

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,27 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+
+const productData = new ProductData("tents");
+console.log(productData);
+const element = document.querySelector(".product-list");
+const productList = new ProductList("tents", productData, element);
+console.log(productList);
+
+
+productList.init();
+
+
+
+
+
+
+
+
+
 const modals = document.getElementById("modal")
 const button = document.getElementById("callBtn")
 const closeButton = document.querySelector("#closeBtn")
-console.log("Test");
+
 
 
 button.addEventListener("click", () => {
